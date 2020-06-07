690eab4fc32fb843eda11e4fc8424ebf498f0d3c
Alina Polupanova <34677605+polu-lina@users.noreply.github.com>
2020-06-07 22:28:39 +0500

    Update TimeServer.py

--- TimeServer.py
+++ TimeServer.py
@@ -6,46 +6,41 @@ class TimeServer():
         self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.test_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server.bind(('localhost', 123))
-        self.server.settimeout(0)
-        self.test_socket.settimeout(0)
+        self.server.settimeout(1)
+        self.test_socket.settimeout(1)
         with open("time.txt", "rb") as f:
             self.time = int(f.readline())
 
-    def send(self):
-        self.test_socket.sendto(bytes(str(time.time()), encoding = "utf-8"), ('localhost', 123))
-
     def take(self):
-        data, addr = self.server.recvfrom(1024)
+        data, address = self.server.recvfrom(512)
         if data:
             difference = time.time() - float(data)
-            self.server.sendto(bytes(time.ctime(time.time() + self.time + difference), encoding = "utf-8"), addr)
+            new_time = time.time() + self.time + difference
+            self.server.sendto(bytes(time.ctime(new_time), encoding = "utf-8"), address)
 
     def check(self):
-        data, addr = self.test_socket.recvfrom(1024)
+        data, _ = self.test_socket.recvfrom(512)
         print("Response: " + str(data))
 
 
 if __name__ == '__main__':
-    timeServer = TimeServer()
-    print("Выберите режим работы:")
-    print("client - клиентский; server - серверный")
-    t = input()
+    server = TimeServer()
+    mode = input("Выберите режим работы: client - клиентский; server - серверный\n")
     try:
-        if t == "client":
+        if mode == "client":
             while True:
                 input("Нажмите enter для отправки запроса")
-                timeServer.send()
-                timeServer.take()
-                timeServer.check()
-        elif t == "server":
+                current_time = str(time.time()
+                server.test_socket.sendto(bytes(current_time), encoding = "utf-8"), ('localhost', 123))
+                server.take()
+                server.check()
+        elif mode == "server":
             while True:
-                timeServer.take()
+                server.take()
         else:
             print("Что-то пошло не так, попробуйте заново")
     except Exception as e:
         print(e)
     finally:
-        timeServer.server.close()
-        timeServer.test_socket.close()
-        time.sleep(1)
-        sys.exit(0)
+        server.server.close()
+        server.test_socket.close()
